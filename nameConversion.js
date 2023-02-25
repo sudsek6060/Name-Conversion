ce25d198603058cfab8e2dcef94d804f8d2949d8
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-02-25 07:02:03 +0530

    Snake Case

--- nameConversion.js
+++ nameConversion.js
@@ -22,3 +22,21 @@ function toCamlecase(str) {
     return camelCase;
 
 }
+
+// Convert To Snake Case
+function toSnakecase(str) {
+    const strSplit = str.split(' ');
+    const lowerStrSplit = strSplit.map(el => el.toLowerCase());
+    const snakeCase = lowerStrSplit.join('_');
+
+    return snakeCase;
+}
+
+// Convert to Screaming Snake Case
+function toScreamingSnakeCase(str) {
+    const strSplit = str.split(' ');
+    const upperrStrSplit = strSplit.map(el => el.toUpperCase());
+    const screamingSnakeCase = upperrStrSplit.join('_');
+
+    return screamingSnakeCase;
+};
